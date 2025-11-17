cf962e8c2fbc839721b0b602ecc020c494447040
Henry Shin <nakada_tokumei@protonmail.com>
2025-11-17 17:16:43 +0900

    [NoneLab] Add config for systemd
    Change-Id: I90afa687f73316d6f16f7bc8abd4d33bb5ff4de6

--- run_qemu.sh
+++ run_qemu.sh
@@ -1,1 +1,2 @@
-sudo qemu-system-x86_64 -m 512 -smp 2 -cpu host -nographic -serial mon:stdio -kernel $1 -enable-kvm -bios /usr/share/ovmf/OVMF.fd
+sudo qemu-system-x86_64 -m 512 -smp 2 -cpu host -nographic -serial mon:stdio -kernel $1 -enable-kvm -bios /usr/share/ovmf/OVMF.fd -append "console=ttyS0 root=/dev/vda rw" \
+	-drive file=$2,format=raw,if=virtio
